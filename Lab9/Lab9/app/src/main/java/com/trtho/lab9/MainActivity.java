5b893c198ac32711de39ae916e7743a0036cfd6c
ThoNT16 <truongthonguyen107@gmail.com>
2025-03-11 23:49:27 +0700

    fix: wrong file after stash [thont]

--- Lab9/Lab9/app/src/main/java/com/trtho/lab9/MainActivity.java
+++ Lab9/Lab9/app/src/main/java/com/trtho/lab9/MainActivity.java
@@ -8,21 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.trtho.lab9.databinding.ActivityMainBinding;
-import com.trtho.lab9.entities.Device;
-
-import java.util.List;
-import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    //private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.deviceFragment,R.id.orderFragment).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_main_activity);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.deviceFragment,R.id.orderFragment).build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_main_activity);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
